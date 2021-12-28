b0601c6177c77721fa487de32632f4702d619ee2
Abdelrahman-Wagdy <wagdy.abdelrahman@gmail.com>
2021-12-28 17:48:35 +0200

    Video Added with background cover

--- src/home_components/home_slider.jsx
+++ src/home_components/home_slider.jsx
@@ -1,8 +1,19 @@
-
+import video from './video1.mp4';
+import audio from './audio1.mp3';
 const HSlider = () => {
+    const playVideo = () =>{
+        document.querySelector(".videoBackground").requestFullscreen();
+        document.querySelector(".videoBackground").play();
+        document.getElementById('audio1').play();
+        document.querySelector(".videoBackground").controls();
+
+    }
     return (  
         <div className="slider section">
-            
+            <video className='videoBackground' width={`100%`} src={video} type="video/mp4" autoPlay={'autoplay'} muted = {'muted'} loop = {'loop'}></video>
+            <div className="cover-layer"></div>
+            <audio id='audio1' src={audio} type="audio/mp3"></audio>
+            <i className="far fa-play-circle play_button-icon" onClick={playVideo}></i>
         </div>
     );
 }
